4e8846c7f887725e9f87f385bf62493420903bbf
Shteren <sht.mor@gmail.com>
2016-02-09 10:22:51 +0200

    some of tables are not been created. now they are

--- WebApp/src/webapp/constants/DBConstants.java
+++ WebApp/src/webapp/constants/DBConstants.java
@@ -11,32 +11,33 @@ public interface DBConstants {
 	//sql create tables statements
 	public final String CREATE_USERS_TABLE = "CREATE TABLE tbl_user (username varchar(10) PRIMARY KEY,"
 			+ "password varchar(8) NOT NULL,"
-			+ "nickname varchar(20),"
+			+ "nickname varchar(20) UNIQUE NOT NULL,"
 			+ "description varchar(50),"
 			+ "photoUrl varchar(200))";
 	public final String CREATE_QUESTIONS_TABLE = "CREATE TABLE tbl_question (questionId INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,"
 			+ "submitionTime TIMESTAMP NOT NULL,"
 			+ "contextText varchar(300) NOT NULL,"
 			+"numberOfVotes INT ,"
-			+ "submmitedUsername varchar(10) REFERENCES tbl_user(username))";
+			+ "submmitedUsername varchar(20) REFERENCES tbl_user(nickname))";
 	public final String CREATE_ANSWERS_TABLE = "CREATE TABLE tbl_answer (answerId INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,"
 			+ "submitionTime TIMESTAMP NOT NULL,"
 			+ "contextText varchar(300) NOT NULL,"
 			+"numberOfVotes INT ,"
 			+"questionId INT REFERENCES tbl_question(questionId),"
-			+ "submmitedUsername varchar(10) REFERENCES tbl_user(username))";
+			+ "submmitedUsername varchar(20) REFERENCES tbl_user(nickname))";
 	public final String CREATE_TOPIC_TABLE = "CREATE TABLE tbl_topic (topicId INT GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,"
 			+ "topicName varchar(100) NOT NULL)";
-		public final String CREATE_REL_QUESTIONS_TOPICS_TABLE = "CREATE TABLE tbl_rel_question_topic (questionId INT REFERENCES tbl_question(questionId),"
+	public final String CREATE_REL_QUESTIONS_TOPICS_TABLE = "CREATE TABLE tbl_rel_question_topic (questionId INT REFERENCES tbl_question(questionId),"
 			+ "topicId INT REFERENCES tbl_topic(topicId),"
 			+ "primary key(questionId,topicId))";
 	public final String CREATE_REL_USER_QUESTIONS_VOTES_TABLE = "CREATE TABLE tbl_rel_user_question_vote (questionId INT REFERENCES tbl_question(questionId),"
 			+ "username varchar(10) REFERENCES tbl_user(username),"
-			+ "primary key(questionId,username))";	
+			+ "primary key(questionId,username),"
+			+ "vote SMALLINT)";
 	public final String CREATE_REL_USER_ANSWERS_VOTES_TABLE = "CREATE TABLE tbl_rel_user_answer_vote (answerId INT REFERENCES tbl_answer(answerId),"
 			+ "username varchar(10) REFERENCES tbl_user(username),"
-			+ "vote SMALLINT CONSTRAINT vote_ck CHECK (vote = 1 or vote = -1),"
-			+ "primary key(answerId,username))";	
+			+ "primary key(answerId,username),"	
+			+ "vote SMALLINT)";
 	public final String INSERT_USER_STMT = "INSERT INTO tbl_user (username,password,nickname,description,photoUrl) VALUES(?,?,?,?,'a')";
 	//public final String SELECT_ALL_USERS_STMT = "SELECT * FROM tbl_user";
 
