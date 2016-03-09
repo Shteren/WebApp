84eaab7d6b135319014c2ba2753caec888224b1d
Shteren <sht.mor@gmail.com>
2016-03-09 23:19:28 +0200

    Revert "bug fix in first answer"
    This reverts commit 3b4e27ee3e86f40b713ac64415cc2a6e0b30990b.

--- WebApp/src/webapp/constants/DBConstants.java
+++ WebApp/src/webapp/constants/DBConstants.java
@@ -38,7 +38,8 @@ public interface DBConstants {
 			+ "username varchar(20) REFERENCES tbl_user(nickname),"
 			+ "primary key(answerId,username),"	
 			+ "vote SMALLINT)";
-	
+	public final String INSERT_USER_STMT = "INSERT INTO tbl_user (username,password,nickname,description,photoUrl) VALUES(?,?,?,?,'a')";
+	//public final String SELECT_ALL_USERS_STMT = "SELECT * FROM tbl_user";
 
 
 }
